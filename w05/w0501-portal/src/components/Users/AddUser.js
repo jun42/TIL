601988a7c397804e43315dbda480226c19f3f5fd
Jun <118579021+jun42@users.noreply.github.com>
2023-08-28 12:10:06 +0900

    48강 ref 사용하기

--- w05/w0501-portal/src/components/Users/AddUser.js
+++ w05/w0501-portal/src/components/Users/AddUser.js
@@ -1,42 +1,39 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from "react";
 
-import Card from '../UI/Card';
-import Button from '../UI/Button';
-import ErrorModal from '../UI/ErrorModal';
-import classes from './AddUser.module.css';
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+    // console.log(nameInputRef.current.value);
+    // console.log(ageInputRef.current.value);
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
-        title: '유효하지 않은 입력값',
-        message: '유효한 나이와 이름을 입력하세요 (비어 있지 않은 값).',
+        title: "유효하지 않은 입력값",
+        message: "유효한 나이와 이름을 입력하세요 (비어 있지 않은 값).",
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
-        title: '유효하지 않은 나이',
-        message: '유효한 나이 값을 입력하세요 (> 0).',
+        title: "유효하지 않은 나이",
+        message: "유효한 나이 값을 입력하세요 (> 0).",
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
-  };
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
+    props.onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   const errorHandler = () => {
@@ -55,19 +52,9 @@ const AddUser = (props) => {
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">사용자명</label>
-          <input
-            id="username"
-            type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
-          />
+          <input id="username" type="text" ref={nameInputRef} />
           <label htmlFor="age">나이</label>
-          <input
-            id="age"
-            type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
-          />
+          <input id="age" type="number" ref={ageInputRef} />
           <Button type="submit">사용자 추가</Button>
         </form>
       </Card>
