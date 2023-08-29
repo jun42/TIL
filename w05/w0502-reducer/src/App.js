628e381d2cbb27a68cacf78820ec8e188486bb5c
Jun <118579021+jun42@users.noreply.github.com>
2023-08-29 19:57:03 +0900

    52강 context API 코드

--- w05/w0502-reducer/src/App.js
+++ w05/w0502-reducer/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import React, { useState } from "react";
+import AuthContext from "./context/AuthContext";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -18,13 +18,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
