90d57181d7b8037af5e980ac79eeca1c00ee199c
Jun <118579021+jun42@users.noreply.github.com>
2023-08-30 09:53:06 +0900

    53강 forwardRef code

---
+++ w05/w0502-forwardRef/src/components/UI/Input/Input.js
@@ -0,0 +1,33 @@
+import React, { forwardRef, useImperativeHandle, useRef } from "react";
+import classes from "../../Login/Login.module.css";
+
+export const Input = forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const active = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: active,
+    };
+  });
+  return (
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
+      <label htmlFor={props.type}>{props.label}</label>
+      <input
+        type={props.type}
+        id={props.type}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+        ref={inputRef}
+      />
+    </div>
+  );
+});
